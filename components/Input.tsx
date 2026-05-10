0a14b35ad535863682a6a7dd801d0236e1af40e9
Dhruv Sharma <bittoos245@gmail.com>
2026-05-10 12:45:07 +0530

    firts and last  commit for StreamFlow project

---
+++ components/Input.tsx
@@ -0,0 +1,64 @@
+import React from "react";
+
+interface Props {
+  id: string;
+  onChange: any;
+  onBlur: any;
+  value: string;
+  label: string;
+  type?: string;
+  error: boolean;
+}
+
+function Input({ id, onChange, onBlur, value, label, type, error }: Props) {
+  return (
+    <div className="relative">
+      <input
+        onChange={onChange}
+        onBlur={onBlur}
+        value={value}
+        type={type}
+        id={id}
+        className={`${error && "bg-[#a74c4c]"}
+        block
+        rounded-md
+        mt-4
+        px-6
+        pt-6
+        pb-1
+        w-full
+      text-white
+      bg-neutral-700
+        appearance-none
+        focus:outline-none
+        focus:ring-0
+        peer`}
+        placeholder=" "
+      />
+      <label
+        htmlFor={id}
+        className="
+        absolute 
+      text-zinc-400
+      text-sm
+        duration-150 
+        transform 
+        -translate-y-3 
+        scale-75 
+        top-7 
+        z-10 
+        origin-[0] 
+        left-4
+        peer-placeholder-shown:scale-100 
+        peer-placeholder-shown:translate-y-0 
+        peer-focus:scale-75
+        peer-focus:-translate-y-3
+      "
+      >
+        {label}
+      </label>
+    </div>
+  );
+}
+
+export default Input;
---
+++ pages/api/auth/[...nextauth].ts
@@ -0,0 +1,45 @@
+import NextAuth from "next-auth";
+import type { NextAuthOptions } from "next-auth";
+import Credentials from "next-auth/providers/credentials";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { compare } from "bcrypt";
+import prismadb from "@/lib/prismadb";
+
+export const authOptions: NextAuthOptions = {
+  providers: [
+    Credentials({
+      id: "credentials",
+      name: "Credentials",
+      credentials: {
+        email: { label: "Email", type: "text" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials) {
+        if (!credentials?.email || !credentials?.password) {
+          throw new Error("Email and password required");
+        }
+        const user = await prismadb.user.findUnique({
+          where: { email: credentials.email },
+        });
+        if (!user || !user.hashedPassword) {
+          throw new Error("Email does not exist");
+        }
+        const isCorrectPassword = await compare(
+          credentials.password,
+          user.hashedPassword
+        );
+        if (!isCorrectPassword) {
+          throw new Error("Incorrect password");
+        }
+        return user;
+      },
+    }),
+  ],
+  pages: { signIn: "/login" },
+  debug: process.env.NODE_ENV === "development",
+  adapter: PrismaAdapter(prismadb),
+  session: { strategy: "jwt" },
+  secret: process.env.NEXTAUTH_SECRET,
+};
+
+export default NextAuth(authOptions);
